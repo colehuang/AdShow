ed2a07cd6634d0a053cc0ef7b13a8d80f5ff9132
bilehai <bilehai@163.com>
2014-12-04 17:11:03 +0800

    add project

---
+++ AdShow/AdShow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AdShow
+//
+//  Created by huang cole on 14-12-4.
+//  Copyright (c) 2014年 huang cole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
